3696c7d442f9f6a18f39c4eff1a1a86fe103e6d4
JuliaIzotova] <yuizotova15@gmail.com>
2022-03-20 19:08:57 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,17 @@ public class Main {
     public static void main(String[] args) {
 
         //Входные данные
-      int userBalance = 100;
-      int income = 1100;
+        int userBalance = 100;
+        int income = 1100;
 
-      // Решение
+        // Решение
         int bonus;
         if (income > 1000) {
             bonus = (int) income / 100;
-        }
-                else {
+        } else {
             bonus = 0;
         }
-                userBalance += income + bonus;
+        userBalance += income + bonus;
         System.out.println("Бонус: ");
         System.out.println(bonus);
         System.out.println("Баланс: ");
